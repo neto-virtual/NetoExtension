7ca9156f0dff6d9d47e46522539888a65b83d181
ThorGalli <gallithor@gmail.com>
2023-12-02 16:30:39 -0300

    Merge image changes into close button changes

--- popup.js
+++ popup.js
@@ -1,13 +1,6 @@
-// popup.js
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-  const currentUrl = tabs[0].url;
-  document.getElementById('currentUrl').textContent = currentUrl;
-
-  const overlay = document.getElementById('overlay');
-  overlay.style.display = 'block';
-
-  const closeBtn = document.getElementById('closeBtn');
-  closeBtn.onclick = function () {
-    overlay.style.display = 'none';
-  };
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+    const currentUrl = tabs[0].url;
+    document.getElementById('currentUrl').textContent = currentUrl;
+  });
 });
